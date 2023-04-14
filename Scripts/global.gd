f2e14f96f2e777197b78c82a54e318dfb83ef1a8
Axel Nunez <axel.z.nunez@gmail.com>
2023-04-13 21:02:31 -0400

    Global.gd: Removed debug print statement

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -11,14 +11,9 @@ var alert_mode: ALERT_MODES = ALERT_MODES.ALERT_OFF:
 var alert_time = 5.0
 var cur_time = 0.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print("Alert Mode: ", alert_mode)
+
 	if alert_mode == ALERT_MODES.ALERT_ON:
 		cur_time += delta
 		if cur_time >= alert_time:
