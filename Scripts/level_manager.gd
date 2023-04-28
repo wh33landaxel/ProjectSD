e2ddffd92f3bc8bdecf59d738a24ad2f940f30be
Axel Nunez <axel.z.nunez@gmail.com>
2023-04-28 15:40:48 -0400

    Level_manager: added levels

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -2,16 +2,19 @@ extends Node
 
 var level_ind = 0
 var level_list = [
-	"res://"
+	"res://Levels/intro.tscn",
+	"res://Levels/world.tscn"
 ]
 
 var delete_on_load = []
 
 func load_next_level():
 	level_ind = (level_ind+1) % level_list.size()
+	load_level(level_ind)
 
 func load_previous_level():
 	level_ind = (level_ind-1) % level_list.size()
+	load_level(level_ind)
 
 func restart_level():
 	for d in delete_on_load:
