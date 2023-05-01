5b20c9baa6e09f16d4d39e818f0965f7f9fdffa8
Axel Nunez <axel.z.nunez@gmail.com>
2023-04-30 23:45:00 -0400

    yaaaaa

--- Scenes/death_area.gd
+++ Scenes/death_area.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,5 +12,11 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
+		TextDisplay.display_text("OUCH!", 2)
+		await get_tree().create_timer(1.0).timeout
+		TextDisplay.hide_display()
 		LevelManager.restart_level()
+		
+		
+		
 
