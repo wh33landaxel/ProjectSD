afeccacaca99564654b607e293a4e81dba05b338
Axel Nunez <axel.z.nunez@gmail.com>
2023-04-30 21:00:21 -0400

    Added cheesy door text triggers

--- TextTrigger.gd
+++ TextTrigger.gd
@@ -6,6 +6,8 @@ enum MOODS { HAPPY, SAD, FREAKED_OUT }
 @export var one_time = true
 var shown = false
 
+#Door Texts
+const DOOR_CLOSED_TEXT = "Sorry bud, can't let you through until you disable the exclamation alarms"
 
 func _ready():
 	connect("body_entered", show_text)
@@ -13,11 +15,18 @@ func _ready():
 	
 
 func show_text(coll):
-	if coll.is_in_group("player"):
+	
+	if get_parent().is_in_group("door"):
+		#Yes this is horrible but game jam
+		if get_parent().door_locked:
+			text_to_show = DOOR_CLOSED_TEXT
+			TextDisplay.display_text(text_to_show, mood_to_show)
+			return
+	elif coll.is_in_group("player") and !get_parent().is_in_group("door"):
 		if one_time and shown:
 			return
-	shown = true
-	TextDisplay.display_text(text_to_show, mood_to_show)
+		shown = true
+		TextDisplay.display_text(text_to_show, mood_to_show)
 
 func hide_text(coll):
 	if coll.is_in_group("player"):
